43f65e0e19f8b1042e6b74e7d299fb3f0be53d35
0Fardin10 <144367663+0Fardin10@users.noreply.github.com>
2025-03-13 14:59:12 +0600

    second massage

---
+++ beginning/Chapter 01  practice/03problem/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float c =37.0,f;
+
+    f=9/5*c+32;
+    printf("The Value in fahrenheit is %f",f);
+
+    //This is my last project of C Fundamental.  InshAllah I will do some more projects in the future, but then I will do it from a new place and keep pace with the university.  New varsity life is about to begin.
+    return 0;
+}
+
